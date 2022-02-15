6bd90ca656b2a505f81dc439f15ccf0dc34d67c0
yogendrapokhrel777@gmail.com <yogendrapokhrel777@gmail.com>
2022-02-15 16:08:37 +0400

    Functions to prepare array from objects and upload files

---
+++ middleware/commonFunctions.js
@@ -0,0 +1,78 @@
+const axios = require('axios');
+var fs = require('fs');
+
+const prepareArrayFromObject = (data) => {
+    let parsedData = [], str = '';
+    
+    if(typeof data == "string"){
+        return [data];
+    }
+
+    data.forEach(function(ele){
+        str = '';
+        if(typeof ele === "string"){
+            parsedData.push(ele);
+        }else{
+            str = extractStringFromObject(ele);
+            parsedData.push(str);
+        }
+    })
+    return parsedData;
+}
+
+const extractStringFromObject = (data) => {
+
+    if(data == null){
+        return '';
+    }
+
+    if(typeof data == "string"){
+        return data;
+    }
+    return extractStringFromObject(Object.values(data)[0]);
+}
+
+ const downloadFiles = function(uri, filename, callback){
+
+    axios({
+        method: 'get',
+        url: uri,
+        responseType: 'stream',
+    }).then(response => {
+
+        return new Promise((resolve, reject) => {
+
+            // fs.writeFile(filename,'',function(){console.log("File created")});
+            var writer = fs.createWriteStream(__basedir+'/public/uploads/'+filename);
+
+            response.data.pipe(writer);
+            let error = null;
+            writer.on('error', err => {
+              error = err;
+              writer.close();
+              reject(err);
+            });
+            writer.on('close', () => {
+              if (!error) {
+                resolve(true);
+              }
+              //no need to call the reject here, as it will have been called in the
+              //'error' stream;
+            });
+        })
+
+
+
+    }).catch(err => {
+        console.log(err)
+    })
+  };
+
+
+module.exports = {
+    prepareArrayFromObject : prepareArrayFromObject,
+    extractStringFromObject: extractStringFromObject,
+    downloadFiles: downloadFiles
+}
+
+// module.export = {prepareArrayFromObject}
