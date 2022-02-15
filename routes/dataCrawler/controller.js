3faf36d797436638c2c210a0e73bf59e141bb710
yogendrapokhrel777@gmail.com <yogendrapokhrel777@gmail.com>
2022-02-15 16:09:47 +0400

    Crawl url updated and function to download file from url added

--- routes/dataCrawler/controller.js
+++ routes/dataCrawler/controller.js
@@ -2,23 +2,33 @@ const express = require('express');
 const router = express.Router();
 const moduleModel  =require('./model');
 const axios = require('axios');
+const {prepareArrayFromObject} = require(__basedir+"/middleware/commonFunctions");
 
 router.get('/',async (req,res) => {
 
-    // var response = await moduleModel.getAllServices(req);
-    // res.send(response);
-    const apiData = await axios({
-        method: 'get',
-        url: 'https://cfrkftig71.execute-api.us-east-1.amazonaws.com/prod?expert=true',
-      })
+    let parsedData = [];
+    let apiData;
+    try{
+        apiData = await axios({
+            method: 'get',
+            url: 'https://cfrkftig71.execute-api.us-east-1.amazonaws.com/prod?expert=true',
+        })
+        parsedData = prepareArrayFromObject(apiData.data);
+    }catch(err){
+        console.log("error on data crawler controller");
+    }
 
-      res.send({msg: "API Data", data: apiData.data});
+    //No need to wait for the execution.
+    moduleModel.uploadFiles(parsedData);
+
+    res.send({msg: "API Data", data: parsedData, apiData: (apiData) ? apiData.data : '' });
    
   })
 
   router.get('/fetch-data',async (req,res) => {
 
-    var response = await moduleModel.getAllServices(req);
+    let filename = uri.split('/').pop();
+    download("https://cdn.reasonsecurity.com/exam-public-file/kiPUbWJY",filename, function(){ console.log("I am uploaded")});
     res.send({msg: "I am fetching data"});
    
   })
--- routes/dataCrawler/model.js
+++ routes/dataCrawler/model.js
@@ -1,9 +1,24 @@
 const models = require(__basedir +'/models');
+var fs = require('fs');
+const {downloadFiles} = require(__basedir+"/middleware/commonFunctions");
 
 class module_model {
-    getAllServices = () =>{
-        return {"msg": "In model"};
+    uploadFiles = async (allFiles) =>{
+        if(allFiles.length > 0){
+            let filename = '';
+            allFiles.forEach(function(uri){
+                if(uri){
+                    filename = uri.split('/').pop();
+                    if(fs.existsSync(__basedir+'/public/uploads/'+filename)){
+                        filename = filename + '_'+ Date.now();
+                    }
+                    downloadFiles(uri,filename, function(){ console.log("File uploaded successfully!")});
+                }
+            })
+        }
     }
+     
+
 }
 
 module.exports = new module_model();
